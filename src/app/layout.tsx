446edd5e7147e6a93476952586a1b06a98eb946b
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-10-07 18:55:01 +0300

    init: next.js project Mon, Oct  7, 2024  6:55:01 PM

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "GetShopTV test task",
+  description: "Customer base monetization",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
